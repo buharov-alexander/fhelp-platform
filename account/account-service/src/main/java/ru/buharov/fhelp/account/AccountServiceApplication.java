9e738ed3b3c41be65c4656c9e614d3c6c056a509
buharov-alexander <buharovsasha@yandex.ru>
2020-06-28 11:11:20 +0700

    account-service: move app class

--- account/account-service/src/main/java/ru/buharov/fhelp/account/AccountServiceApplication.java
+++ account/account-service/src/main/java/ru/buharov/fhelp/account/AccountServiceApplication.java
@@ -1,4 +1,4 @@
-package ru.buharov.fhelp.accountservice;
+package ru.buharov.fhelp.account;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;

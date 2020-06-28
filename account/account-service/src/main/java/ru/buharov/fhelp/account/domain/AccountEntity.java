4bfceef39a30a87518ba2f90b6ada51b369edfa2
buharov-alexander <buharovsasha@yandex.ru>
2020-06-28 11:10:52 +0700

    account-service: add account entity

---
+++ account/account-service/src/main/java/ru/buharov/fhelp/account/domain/AccountEntity.java
@@ -0,0 +1,35 @@
+package ru.buharov.fhelp.account.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor(access = AccessLevel.PACKAGE)
+@AllArgsConstructor(access = AccessLevel.PACKAGE)
+@Entity
+@Table(name = "accounts")
+public class AccountEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private AccountTypeEnum type;
+
+    @Enumerated(EnumType.STRING)
+    private ValutaEnum valuta;
+
+    private String owner;
+}
---
+++ account/account-service/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+-- Create test accounts
+INSERT INTO accounts (name, type, valuta, owner) VALUES ('Cash rubs', 'CASH', 'RUB', 'root');
+INSERT INTO accounts (name, type, valuta, owner) VALUES ('Alfa card rubs', 'BANK_ACCOUNT', 'RUB', 'root');

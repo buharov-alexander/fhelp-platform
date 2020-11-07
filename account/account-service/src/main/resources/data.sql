4090260f740e3829c9cc83703b4c96ab027f87dc
buharov-alexander <buharovsasha@yandex.ru>
2020-11-07 22:40:23 +0700

    account: fix test data

--- account/account-service/src/main/resources/data.sql
+++ account/account-service/src/main/resources/data.sql
@@ -3,5 +3,5 @@ INSERT INTO account_states (id, balance, modified, comment) VALUES ('b5607d38-8f
 INSERT INTO account_states (id, balance, modified, comment) VALUES ('e33962a0-d841-48b1-8f43-caf98116f3ee', 150000, '2019-01-02', 'Init state');
 
 -- Create test accounts
-INSERT INTO accounts (name, type, valuta, state_id) VALUES ('Cash rubs', 'CASH', 'RUB', 'b5607d38-8fc1-43ef-b44e-34967083c80a');
-INSERT INTO accounts (name, type, valuta, state_id) VALUES ('Alfa card rubs', 'BANK_ACCOUNT', 'RUB', 'e33962a0-d841-48b1-8f43-caf98116f3ee');
+INSERT INTO accounts (id, name, type, valuta, state_id) VALUES ('2305e3e4-7aca-4e5f-ad14-68bf64281a0e', 'Cash rubs', 'CASH', 'RUB', 'b5607d38-8fc1-43ef-b44e-34967083c80a');
+INSERT INTO accounts (id, name, type, valuta, state_id) VALUES ('517441e1-928b-4405-92ee-abe645347df8', 'Alfa card rubs', 'BANK_ACCOUNT', 'RUB', 'e33962a0-d841-48b1-8f43-caf98116f3ee');

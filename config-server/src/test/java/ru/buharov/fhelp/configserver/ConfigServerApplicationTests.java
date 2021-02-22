0e45086fcf9cc2d74404dbdca1b35f0d4c4d95f5
Alexander Bukharov <buharovsasha@yandex.ru>
2021-02-22 22:46:05 +0700

    config: init commit

---
+++ config-server/src/test/java/ru/buharov/fhelp/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.buharov.fhelp.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

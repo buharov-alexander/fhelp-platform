aefe90a385398082f2560d3f13b670c5ed95a5d1
Alexander Bukharov <buharovsasha@yandex.ru>
2021-04-04 17:35:19 +0700

    frontend: add bar

---
+++ fhelp-frontend/src/components/AppBar.vue
@@ -0,0 +1,5 @@
+<template>
+  <v-app-bar app color="deep-purple" dark>
+    <v-toolbar-title>Title</v-toolbar-title>
+  </v-app-bar>
+</template>
---
+++ fhelp-frontend/src/components/Drawer.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-navigation-drawer v-model="drawer" permanent mini-variant="">
+    <v-list nav dense>
+      <v-list-item-group>
+        <v-list-item>
+          <v-list-item-icon>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-icon>
+        </v-list-item>
+
+        <v-list-item>
+          <v-list-item-icon>
+            <v-icon>mdi-account</v-icon>
+          </v-list-item-icon>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    drawer: true,
+  }),
+};
+</script>
